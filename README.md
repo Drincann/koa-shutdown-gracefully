3b4e9ad754083fea2a1e9baa721b2e136e1b3048
Drincann <1019933576@qq.com>
2022-05-09 23:52:31 +0800

    init

---
+++ README.md
@@ -0,0 +1,39 @@
+# Koa shutdown gracefully
+
+用来对服务器进行优雅停机，以平滑重启 Koa 应用。
+
+## example
+
+当 Node.js 应用收到 SIGINT 信号后，直到应用关闭前，其行为是向所有新请求返回 503 状态.
+
+```js
+const graceful = require('koa-shutdown-graceful');
+const app = require('koa')();
+
+app.use(shutdownGraceful())
+  .use(/* your services */)
+  .listen(/* port */);
+```
+
+你可以用下面的代码来测试:
+
+!注意，使用同一浏览器进程，即使在不同标签下同时访问仍会拥塞，它们使用的应该是同一个 socket 连接。
+
+你可以尝试使用不同的浏览器来同时访问和测试，它们会使用本地不同的端口号。
+
+```js
+const app = require('koa')();
+
+app.use(shutdownGraceful())
+  .use(async (ctx,) => {
+    await new Promise(r => setTimeout(() => r(null), 10000))
+    ctx.set('Content-Type', 'text/html')
+    ctx.body = '<h1>hello world</h1>'
+  }).listen(80)
+```
+
+## build
+
+```sh
+npm run build # npx tsc index.ts --outDir dist
+```
---
+++ index.ts
@@ -0,0 +1,33 @@
+import * as Koa from 'koa'
+import * as process from 'process'
+
+export function shutdownGraceful() {
+  const reqSet = new Set<ReturnType<Koa.Next>>()
+  let exiting = false
+  process.on('SIGINT', async () => {
+    exiting = true
+    console.log('SIGINT')
+    console.log(reqSet);
+
+    await Promise.all(Array.from(reqSet))
+    setImmediate(() => {
+      process.exit(0)
+    })
+  })
+
+  async function handleRequest(next: Koa.Next) {
+    const handlePromise = next()
+    reqSet.add(handlePromise)
+    await handlePromise
+    reqSet.delete(handlePromise)
+  }
+
+  return async (ctx: Koa.Context, next: Koa.Next) => {
+    if (exiting) {
+      ctx.status = 503
+      return
+    }
+    console.log(reqSet)
+    await handleRequest(next)
+  }
+}
